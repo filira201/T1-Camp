c7a03d05239dd68c8f1b965c6055f67c7d7a32d0
Egor <egor297198@mail.ru>
2025-07-16 20:42:16 +0400

    add burger menu, for header on mobile devices

--- First-Task-Manager/client/src/components/Header.tsx
+++ First-Task-Manager/client/src/components/Header.tsx
@@ -3,15 +3,18 @@ import {
   NavbarBrand,
   NavbarContent,
   NavbarItem,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle,
   Switch,
 } from "@heroui/react";
 import { FaRegMoon } from "react-icons/fa";
 import { LuSunMedium } from "react-icons/lu";
-
 import { useState } from "react";
 import { useAppDispatch, useAppSelector } from "../hooks";
 import { toggleTheme } from "../store/reducers/themeSlice";
-import { Link } from "react-router";
+import { Link, NavLink } from "react-router";
+import classNames from "classnames";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
@@ -29,12 +32,28 @@ const Header = () => {
       isMenuOpen={isMenuOpen}
       onMenuOpenChange={setIsMenuOpen}
     >
-      <NavbarContent justify="start">
+      <NavbarContent className="xs:hidden" justify="start">
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Закрыть меню" : "Открыть меню"}
+        />
+        <NavbarBrand>
+          <Link
+            onClick={() => setIsMenuOpen(false)}
+            aria-label="Перейти на главнаю страницу"
+            to="/"
+            className="font-bold text-inherit transition-colors hover:text-foreground-500 focus-visible:outline-primary"
+          >
+            TM
+          </Link>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden xs:flex" justify="start">
         <NavbarBrand>
           <Link
             aria-label="Перейти на главнаю страницу"
             to="/"
-            className="font-bold text-inherit transition-colors hover:text-foreground-500 active:text-foreground-400 focus-visible:outline-blue-500"
+            className="font-bold text-inherit transition-colors hover:text-foreground-500 focus-visible:outline-primary"
           >
             TM
           </Link>
@@ -42,6 +61,22 @@ const Header = () => {
       </NavbarContent>
 
       <NavbarContent justify="end">
+        <NavbarItem className="hidden xs:block">
+          <NavLink
+            to="/task/new"
+            //TODO: МБ додумать покрасивее классы, отдельные компоненты для NavBarLink
+            className={({ isActive }) =>
+              classNames(
+                "font-medium transition-colors hover:text-blue-500",
+                {
+                  "text-blue-500": isActive,
+                }
+              )
+            }
+          >
+            Создать задачу
+          </NavLink>
+        </NavbarItem>
         <NavbarItem>
           <Switch
             isSelected={!darkMode}
@@ -52,6 +87,26 @@ const Header = () => {
           />
         </NavbarItem>
       </NavbarContent>
+
+      <NavbarMenu className="flex items-center">
+        <NavbarMenuItem>
+          <NavLink
+            to="/task/new"
+            onClick={() => setIsMenuOpen(false)}
+            //TODO: МБ додумать покрасивее классы
+            className={({ isActive }) =>
+              classNames(
+                "font-medium transition-colors hover:text-blue-500",
+                {
+                  "text-blue-500": isActive,
+                }
+              )
+            }
+          >
+            Создать задачу
+          </NavLink>
+        </NavbarMenuItem>
+      </NavbarMenu>
     </Navbar>
   );
 };
